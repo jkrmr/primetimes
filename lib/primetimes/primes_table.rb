f08b14494b3b9bdc615149ca6e3fa41b0ee5d839
Jake Romer <jkrmr.io@gmail.com>
2017-05-26 16:42:55 -0400

    Add some light documentation

--- lib/primetimes/primes_table.rb
+++ lib/primetimes/primes_table.rb
@@ -23,7 +23,34 @@ module Primetimes
     private
 
     def _table
-      rows = primes.map do |prime_a|
+      rows = _inner_table
+
+      # add left-header column of the list of primes
+      primes.zip(rows).each do |prime, products|
+        products.unshift(prime)
+      end
+
+      # add top-header row of the list of primes
+      rows.unshift(primes.unshift(nil))
+    end
+
+    # Map a list of primes to a list of lists, each list being a list of
+    # products of primes. Products are cached in the `products` hash table.
+    #
+    # Side Effect:
+    #   Set the maximum product encountered as a property (`max_product`) on the
+    #   object.
+    #
+    # Example:
+    #   In: [2, 3, 5, 7]
+    #
+    #   Out: [[ 4,  6, 10, 14],
+    #         [ 6,  9, 15, 21],
+    #         [10, 15, 25, 35],
+    #         [14, 21, 35, 49]]
+    #
+    def _inner_table
+      primes.map do |prime_a|
         primes.map do |prime_b|
           tuple = [prime_a, prime_b].sort
 
@@ -38,12 +65,6 @@ module Primetimes
           products[tuple]
         end
       end
-
-      primes.zip(rows).each do |prime, products|
-        products.unshift(prime)
-      end
-
-      rows.unshift(primes.unshift(nil))
     end
   end
 end
