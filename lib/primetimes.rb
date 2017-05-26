38e39903545eb41dc45dd3cb27023ce1b097357e
Jake Romer <jkrmr.io@gmail.com>
2017-05-26 03:24:51 -0400

    Add reqs and stubs to top-level module

--- lib/primetimes.rb
+++ lib/primetimes.rb
@@ -2,4 +2,10 @@
 
 module Primetimes
   module Primality; end
+  class PrimesTable; end
+  class PrimesTablePrinter; end
 end
+
+require "primetimes/primality"
+require "primetimes/primes_table"
+require "primetimes/primes_table_printer"
