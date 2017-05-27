5e312196e1c28eaee722d730f4948f834c8dc740
Jake Romer <jkrmr.io@gmail.com>
2017-05-27 14:54:24 -0400

    Set max_product_length as a property on printer
    ```
    n: 100    0.200000   0.020000   0.220000 (  0.222870)
    ```

--- lib/primetimes/primes_table_printer.rb
+++ lib/primetimes/primes_table_printer.rb
@@ -2,7 +2,7 @@
 
 module Primetimes
   class PrimesTablePrinter
-    attr_reader :output, :cell_string
+    attr_reader :output, :cell_string, :max_product_length
 
     def initialize(output = $stdout)
       @output = output
@@ -10,17 +10,18 @@ module Primetimes
     end
 
     def print(table)
-      table_rows = table.to_a.map { |row| row_string(row, table).join(" ") }
+      @max_product_length = table.max_product_length
+      table_rows = table.to_a.map { |row| row_string(row).join(" ") }
       table_rows << ""
       output << table_rows.join("\n")
     end
 
     private
 
-    def row_string(row, table)
+    def row_string(row)
       row.map do |cell|
         unless cell_string[cell]
-          cell_string[cell] = cell.to_s.rjust(table.max_product_length)
+          cell_string[cell] = cell.to_s.rjust(max_product_length)
         end
 
         cell_string[cell]
