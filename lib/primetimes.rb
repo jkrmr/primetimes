5937fc38599ed03bd5e64f5e4332d0c9bda74298
Jake Romer <jkrmr.io@gmail.com>
2017-05-27 08:00:32 -0400

    Simplify script logic

--- lib/primetimes.rb
+++ lib/primetimes.rb
@@ -4,3 +4,10 @@ require "primetimes/primality"
 require "primetimes/primes_list"
 require "primetimes/primes_table"
 require "primetimes/primes_table_printer"
+
+module Primetimes
+  def self.print(num_of_primes)
+    table = PrimesTable.new(n: num_of_primes)
+    PrimesTablePrinter.new.print(table)
+  end
+end
